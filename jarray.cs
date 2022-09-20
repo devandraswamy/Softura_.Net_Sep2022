946a01e751afb32dcec2c338ddf0015582512c11
devandraswamy <93170611+devandraswamy@users.noreply.github.com>
2022-09-20 21:06:34 +0530

    Add files via upload

---
+++ jarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractclass1
+{
+    internal class jarray
+    {
+        public static void Main()
+        {
+            string[][] patients = new string[3][];
+            patients[0] = new string[] { "Jay", "Kay", "Ray", "May" };
+            patients[1] = new string[] { "Tommy", "Jonny" };
+            patients[2] = new string[] { "Sita", "Lakshmi", "Riya", "Priya", "Ram" };
+            for (int i = 0; i < patients.Length; i++)
+            {
+                for (int j = 0; j < patients[i].Length; j++)
+                {
+                    Console.Write(patients[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
