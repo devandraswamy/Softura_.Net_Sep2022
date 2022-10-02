db1ebad073902f2e7f6b3bb35fe7bde45f019c25
devandraswamy <93170611+devandraswamy@users.noreply.github.com>
2022-10-02 00:22:41 -0700

    Here I'm uploaded mvc Task 1 & 2
    Created a new controller ,modal ,view. and accessed data in modal through view

---
+++ data.cs
@@ -0,0 +1,14 @@
+﻿namespace demo.Models
+{
+    public class data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+
+        public string Location { get; set; }=string.Empty ;
+    }
+
+ 
+    
+   
+}
---
+++ welcomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using demo.Models;
+
+namespace demo.Controllers
+{
+    public class welcomeController : Controller
+    {
+        public  string game(string name,int id=1)
+        {
+            return ($"Im Welcome {name} Controller {id}");
+        }
+
+        public IActionResult Details(string name, int visttime=1)
+        {
+            ViewData["n"] = "Welcome "+name;
+            ViewData["vt"]= visttime;
+            return View();
+        }   
+        public IActionResult info()
+        {
+            data d = new data();
+            d.Name = "Devandarsawmy";
+            d.Id = 5;
+            d.Location = "Tamilnadu";
+            return View(d);
+        }
+
+    }
+}
