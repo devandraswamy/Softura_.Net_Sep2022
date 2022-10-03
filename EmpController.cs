936d5b74364fcbe88e7cc8219d9bf0b64a6f03b7
devandraswamy <93170611+devandraswamy@users.noreply.github.com>
2022-10-03 20:34:11 +0530

    here i enclosed my today's task
    attached controller, modal, and view files here

---
+++ EmpController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvctask.Models;
+
+namespace Mvctask.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Singleemp()
+        {
+            List<Empdata> list1 = Empdata.getsingle();
+            return View(list1);
+        }
+
+        public IActionResult Allemp()
+        {
+            List<Empdata> list=Empdata.Getallempdata();
+            return View(list);
+        }
+
+        public IActionResult insertdata()
+        {
+            return View();
+        }
+    }
+}
---
+++ Empdata.cs
@@ -0,0 +1,38 @@
+﻿using Mvctask.Controllers;
+
+namespace Mvctask.Models
+{
+    public class Empdata
+    {
+        public int EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+
+        public string gender { get; set; }
+
+
+        public static List<Empdata> Getallempdata()
+        {
+            List<Empdata> edata = new()
+            {
+                new Empdata(){EmpId=1,FirstName="devandra",lastName="swamy",gender="male"},
+                new Empdata(){EmpId=2,FirstName="samual",lastName="rajandran",gender="male"},
+                new Empdata(){EmpId=3,FirstName="siva",lastName="priya",gender="Female"},
+                new Empdata(){EmpId=4,FirstName="swathi",lastName="priya",gender="Female"},
+                new Empdata(){EmpId=5,FirstName="Ajay",lastName="Reyan",gender="male"},
+                new Empdata(){EmpId=6,FirstName="jay",lastName="kishan",gender="male"}
+            };
+            return edata;
+        }
+
+        public static List<Empdata> getsingle()
+        {
+            List<Empdata> es = new()
+            {
+                new Empdata() {EmpId=10,FirstName="sinle",lastName="emp",gender="male"}
+               
+            };
+            return es;
+        }
+    }
+}
